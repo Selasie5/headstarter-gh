b66479c32f0613f8e6176c304d2e168c558ee87e
Selasie5 <selasisepenu5@gmail.com>
2025-01-05 01:16:51 +0000

    Initial commit

---
+++ app/components/ui/Card.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface CardProps {
+  svgComponent: React.ReactNode; // Correct type for an SVG component
+  cardTitle: string;
+  cardContent: string;
+}
+
+const Card: React.FC<CardProps> = ({ svgComponent, cardTitle, cardContent }) => {
+  return (
+    <div className="border-2 border-gray-500 flex flex-col justify-center items-center rounded-lg px-4 py-6 w-72 h-96 space-y-12">
+       <div className="mb-3 text-lime-400 bg-lime-400/30  p-4 rounded-lg">{svgComponent}</div>
+      <div>
+        <h3 className="text-white text-2xl font-semibold mb-2 text-center">{cardTitle}</h3>
+        <p className="text-gray-200 text-center">{cardContent}</p>
+        </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ app/components/ui/EventCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const EventCard =({ image, title }: { image: string; title: string }) => {
+  return (
+    <div
+    className="relative w-full h-64 bg-cover bg-center rounded-lg overflow-hidden"
+    style={{ backgroundImage: `url(${image})` }}
+  >
+    <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+      <h2 className="text-white text-xl md:text-2xl font-semibold">{title}</h2>
+    </div>
+  </div>
+  )
+}
+
+export default EventCard
